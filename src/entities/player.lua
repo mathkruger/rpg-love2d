a11488e2b4607fca1f965e0a0307fc7cdf6ed5f4
Matheus Kruger <ms-kruger@hotmail.com>
2025-05-14 23:37:48 -0300

    adding walking particles

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -2,26 +2,50 @@ local anim8 = require "res.lib.anim8"
 
 love.graphics.setDefaultFilter("nearest", "nearest")
 
+local function getBubble(size)
+    local bubble = love.graphics.newCanvas(size, size)
+    love.graphics.setCanvas(bubble)
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.rectangle("fill", 0, 0, size, size)
+    love.graphics.setCanvas()
+    return bubble
+end
+
+local function getBubbleTrail(image)
+    pSystem = love.graphics.newParticleSystem(image, 50)
+    pSystem:setParticleLifetime(0.5, 0.5)
+    pSystem:setSpeed(-10)
+    pSystem:setColors(255, 255, 255, 200, 255, 255, 255, 100, 255, 255, 255, 0)
+    pSystem:setSizes(0.2, 0.8)
+    return pSystem
+end
+
 Player = {}
 
-function Player:create(collider)
+function Player:create(world, gameMap)
     self.__index = self
 
-    self.x = 45
-    self.y = 640
+    self.x = 0
+    self.y = 0
     self.accelereration = 300
+    self.width = 12
+    self.height = 18
 
-    self.collider = collider
+    self.collider = world:newBSGRectangleCollider(gameMap.map.properties["playerStartX"],
+        gameMap.map.properties["playerStartY"], 50, 100, 10)
     self.collider:setFixedRotation(true)
 
     self.spriteSheet = love.graphics.newImage("res/sprites/player-sheet.png")
-    self.grid = anim8.newGrid(12, 18, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
-    self.animations = {}
-    self.animations.up = anim8.newAnimation(self.grid("1-4", 4), 0.1)
-    self.animations.left = anim8.newAnimation(self.grid("1-4", 2), 0.1)
-    self.animations.down = anim8.newAnimation(self.grid("1-4", 1), 0.1)
-    self.animations.right = anim8.newAnimation(self.grid("1-4", 3), 0.1)
-    self.anim = self.animations.down
+    self.grid = anim8.newGrid(self.width, self.height, self.spriteSheet:getWidth(), self.spriteSheet:getHeight())
+    self.animations = {
+        up = anim8.newAnimation(self.grid("1-4", 4), 0.2),
+        left = anim8.newAnimation(self.grid("1-4", 2), 0.2),
+        down = anim8.newAnimation(self.grid("1-4", 1), 0.2),
+        right = anim8.newAnimation(self.grid("1-4", 3), 0.2)
+    }
+    self.anim = self.animations.right
+
+    self.walkParticles = getBubbleTrail(getBubble(30))
 
     return self
 end
@@ -35,24 +59,28 @@ function Player:update(dt)
     if love.keyboard.isDown("w") then
         vy = -1 * self.accelereration
         self.anim = self.animations.up
+        self.walkParticles:setDirection(math.rad(-90))
         isMoving = true
     end
 
     if love.keyboard.isDown("a") then
         vx = -1 * self.accelereration
         self.anim = self.animations.left
+        self.walkParticles:setDirection(math.rad(180))
         isMoving = true
     end
 
     if love.keyboard.isDown("s") then
         vy = self.accelereration
         self.anim = self.animations.down
+        self.walkParticles:setDirection(math.rad(270))
         isMoving = true
     end
 
     if love.keyboard.isDown("d") then
         vx = self.accelereration
         self.anim = self.animations.right
+        self.walkParticles:setDirection(0)
         isMoving = true
     end
 
@@ -60,18 +88,27 @@ function Player:update(dt)
 
     if isMoving == false then
         self.anim:gotoFrame(2)
+        self.walkParticles:setEmissionRate(0)
+    else
+        self.walkParticles:setEmissionRate(4)
     end
+        
 
     self.anim:update(dt)
 
     self.x = self.collider:getX()
     self.y = self.collider:getY()
+
+    self.walkParticles:setPosition(self.x, self.y + self.height * 2)
+    self.walkParticles:update(dt)
 end
 
 function Player:moveTo(x, y)
-    self.x, self.y = x, y
+    self.collider:setX(x)
+    self.collider:setY(y)
 end
 
 function Player:draw()
-    self.anim:draw(self.spriteSheet, self.x, self.y, nil, 6, nil, 6, 9)
+    love.graphics.draw(self.walkParticles, 0, 0)
+    self.anim:draw(self.spriteSheet, self.x, self.y, nil, 6, nil, self.width / 2, self.height / 2)
 end
